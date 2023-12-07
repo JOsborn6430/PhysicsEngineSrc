5ecff99ea561b474c9d6f6f39044e1d96c3aa66c
JOsborn6430 <JOSBORN6430@smail.pcd.edu>
2023-12-07 11:53:15 -0800

    test

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Please work");
+        System.out.println("I think this works");
 
 
         //Create and initialize Objects
